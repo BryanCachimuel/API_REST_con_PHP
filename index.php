6d142a0d345aacd58452ce3d29374854e7d05fbe
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-12 22:17:59 -0500

    estructurando el proyecto para hacer la API REST con PHP

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+  require_once "controladores/rutas.controlador.php";
+  
+  // instancia de la clase creada en el archivo rutas.controlador.php
+  $rutas = new ControladorRutas();
+  $rutas->inicio();
+
+?>
