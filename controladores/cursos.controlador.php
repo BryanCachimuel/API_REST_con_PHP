8dfaf2e4f8aed9c84da3f9e3b2fe9b96d7d1c1d2
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-13 19:56:10 -0500

    creando los métodos CRUD para los cursos con sus respectivas tutas y métodos en el controlador de cursos

--- controladores/cursos.controlador.php
+++ controladores/cursos.controlador.php
@@ -20,6 +20,33 @@ class ControladorCursos{
           return;
     }
 
+    public function ver($id){
+        $json=array(
+            "detalle"=>"este es el curso con el id: ".$id
+          );
+    
+          echo json_encode($json,true);
+          return;
+    }
+
+    public function actualizar($id){
+        $json=array(
+            "detalle"=>"curso actualizado correctamente con el id: ".$id
+          );
+    
+          echo json_encode($json,true);
+          return;
+    }
+
+    public function eliminar($id){
+        $json=array(
+            "detalle"=>"curso eliminado correctamente con el id: ".$id
+          );
+    
+          echo json_encode($json,true);
+          return;
+    }
+
 }
 
 ?>
--- rutas/rutas.php
+++ rutas/rutas.php
@@ -49,6 +49,35 @@ if(count(array_filter($arrayRutas)) == 1){
       }
     }
 
+ }else{
+  /* estamos evaluando si la ruta esta en el indice 2 que es cursos se lea también que exista un numero en el indice 3 */
+  if(array_filter($arrayRutas)[2] == "cursos" && is_numeric(array_filter($arrayRutas)[3])){
+    
+    /*=======================================
+      Peticion GET
+    ========================================*/
+    if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "GET"){
+      $cursos = new ControladorCursos;
+      $cursos->ver(array_filter($arrayRutas)[3]);
+    }
+
+    /*=======================================
+      Peticion PUT
+    ========================================*/
+    if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "PUT"){
+      $editarcursos = new ControladorCursos;
+      $editarcursos->actualizar(array_filter($arrayRutas)[3]);
+    }
+
+    /*=======================================
+      Peticion DELETE
+    ========================================*/
+    if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "DELETE"){
+      $eliminarcursos = new ControladorCursos;
+      $eliminarcursos->eliminar(array_filter($arrayRutas)[3]);
+    }
+  }
+
  }
 }
 
