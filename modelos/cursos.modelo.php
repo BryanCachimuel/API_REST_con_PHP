7714a5fee6017f76eb814c1344c351af6679ed99
BryanCachimuel <bryanloyo56@gmail.com>
2022-10-17 20:36:48 -0500

    creando el método de eliminar en el archivo cursos.modelo el cual hace la consulta hacia la base de datos sobre el curso que desea eliminar mediante su id

--- modelos/cursos.modelo.php
+++ modelos/cursos.modelo.php
@@ -60,7 +60,20 @@ class ModeloCursos{
 		}
 		$stmt-> close();
 		$stmt = null;
-	} 
+	}
+	
+	static public function eliminar($tabla, $id){
+		$stmt=Conexion::conectar()->prepare("DELETE  FROM $tabla WHERE id=:id");
+        $stmt -> bindParam(":id", $id, PDO::PARAM_INT);
+
+		if($stmt->execute()){
+            return "ok";
+        }else{
+           print_r(Conexion::conectar()->errorInfo());
+        }
+	    $stmt-> close();
+		$stmt = null;
+	}
 }
 
 ?>
